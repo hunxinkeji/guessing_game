5b1f4845114733c42f89c996bcfec867ab8e0722
mask <87345358@qq.com>
2019-08-27 11:46:18 +0800

    src commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1,100);
+
+    loop {
+        println!("请输入你猜的数字");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess :u32 = match  guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("你猜的数字是: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了!"),
+            Ordering::Greater => println!("太大了!"),
+            Ordering::Equal => {
+                println!("你赢了!");
+                break;
+            },
+        }
+    }
+}
